2e207decdda9da1d351deb6dac2e440512f292ea
Amir <amirbaymagambetov14@gmail.com>
2023-02-18 13:11:43 +0600

    lab 4

---
+++ math.py
@@ -0,0 +1,23 @@
+import math
+
+# 1
+# n = int(input())
+# print(math.radians(n))
+
+# 2
+# h = int(input())
+# a = int(input())
+# b = int(input())
+# print((a + b) / 2 * h)
+
+# 3
+# n = int(input())
+# s = int(input())
+# p = n * s
+# a = s / (2 * math.tan(math.pi / n))
+# print(p * a / 2)
+
+# 4
+# a = float(input())
+# b = float(input())
+# print(a * b)
